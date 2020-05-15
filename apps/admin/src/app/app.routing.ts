94783a16b949f78d546d472a0a2c01279ad5ee05
thanhoangz <xuanhoang.ks6@gmail.com>
2020-05-15 16:06:06 +0700

    update layout v2

--- apps/admin/src/app/app.routing.ts
+++ apps/admin/src/app/app.routing.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './business-modules/login/login.component';
 import { PageNotFoundComponent } from 'libs/ui/src/lib/page-not-found/page-not-found.component';
 import { SharedModule } from './shared/modules/shared/shared.module';
+import { LayoutComponent } from './business-modules/layout/layout.component';
 
 const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'manager', component: LayoutComponent },
   { path: '404', component: PageNotFoundComponent },
 ];
 
---
+++ apps/admin/src/app/business-modules/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lottery-workspace-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
